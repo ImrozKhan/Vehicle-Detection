d9b92702225c9f719522b38fb5e30bd21e06a941
Imroz Khan <imrozkhan111@gmail.com>
2024-07-06 00:22:39 +0530

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -217,7 +217,7 @@ class ComputeLoss:
             a = t[:, 6].long()  # anchor indices
             print(gain)
             print(indices)
-            indices.append((b, a, gj.clamp_(0, gain[3] - 1).long(), gi.clamp_(0, gain[2] - 1).long()))  # image, anchor, grid indices
+            indices.append((b, a, gj.clamp_(0, gain[3].long() - 1), gi.clamp_(0, gain[2].long() - 1)))  # image, anchor, grid indices
             tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
             anch.append(anchors[a])  # anchors
             tcls.append(c)  # class
